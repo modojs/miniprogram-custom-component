366423ecb7f91d0dbc2ac570e5e3db428e50fbbc
scottjeremy <scottjeremy1006@gmail.com>
2018-11-27 17:36:42 +0800

    :zap: es6 to es5

--- src/index.ts
+++ src/index.ts
@@ -2,23 +2,23 @@ Component({
   properties: {
     prop: {
       type: String,
-      value: '你不知道的事情还有很多'
+      value: 'some test ts'
     },
   },
   lifetimes: {
     attached() {
       let that = this;
       wx.getSystemInfo({
-        success(res) {
+        success(res: any) {
           // eslint-disable-next-line no-console
           console.log(res);
-          that.showSomeThing('asdasd');
+          that.showSomeThing('js');
         }
       })
     }
   },
   methods: {
-    showSomeThing(title) {
+    showSomeThing(title: string) {
       console.log('you dont know', title);
     }
   }
