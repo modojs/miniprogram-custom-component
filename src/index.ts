a34e39e3784367e149b9d13948b748eafb427a39
liangkaikuo <liangkaikuo@pinquest.cn>
2019-04-22 16:26:46 +0800

    switch

--- src/index.ts
+++ src/index.ts
@@ -6,23 +6,12 @@ Component({
     },
   },
   lifetimes: {
-    attached() {
-      let that = this;
-      wx.getSystemInfo({
-        success(res) {
-          // eslint-disable-next-line no-console
-          console.log(res);
-          that.showSomeThing('asdasd');
-        }
-      })
-    }
   },
   methods: {
-    showSomeThing(title) {
-      console.log('you dont know', title);
-    },
     handleToggle() {
-      this.isOpen = !this.Open;
+      this.setData({
+        isOpen: !this.data.isOpen
+      });
     }
   }
 });
